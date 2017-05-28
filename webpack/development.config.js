a338512174a1bf41c3fd8c4d9bc75127fd76edb9
hazratgs <hazratgs@gmail.com>
2017-05-28 21:46:40 +0300

    Добавили к developer конфигурации devtool с значение cheap-module-eval-source-map для дебагинга

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:3000',
