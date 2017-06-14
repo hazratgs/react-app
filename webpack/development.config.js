57d00cf5269d632627b0c85bf8b3d0be9d4d5ee5
hazratgs <hazratgs@gmail.com>
2017-06-14 22:09:02 +0300

    Исправил ошибку мерджа Object и Array dev конфига

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -4,11 +4,13 @@ const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server'
-  ],
+  entry: {
+    hrm: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://0.0.0.0:3000',
+      'webpack/hot/only-dev-server'
+    ]
+  },
   module: {
     rules: [
       {
