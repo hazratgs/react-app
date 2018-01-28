d9bb2cedc452cc3cec425e0432218a527a364f30
hazratgs <hazratgs@gmail.com>
2018-01-28 20:11:51 +0300

    Добавил Декораторы (stage-2), но пока ESlint ругается на него

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,11 @@
+// eslint-disable-line
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux' // eslint-disable-line
+import { withRouter } from 'react-router-dom' // eslint-disable-line
 import s from './style.pcss'
 
+@connect(state => ({ state: state }))
+@withRouter
 export default class Home extends PureComponent {
   render () {
     return (
